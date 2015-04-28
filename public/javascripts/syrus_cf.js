38ab22b5c7469d24c0b299242d92708db4b6f7ae
Mirko <mirkos93@gmail.com>
2015-04-28 09:51:44 +0200

    Upgrade with configuration

--- public/javascripts/syrus_cf.js
+++ public/javascripts/syrus_cf.js
@@ -1,14 +1,32 @@
 CloudFlare.define("syrus",
     [       "syrus/config", "cloudflare/jquery1.7"],
     function(_config,$)
-    {		
-		function addRemarketing(){
-			var protocol = window.location.protocol != "https:" ? "http:" : "https:";
-			$("body").append(remarketing_code);
+    {
+		function googleTrackingPixel(config) {
+            window.google_conversion_id = config.conversion_id;
+            window.google_conversion_language = "en";
+            window.google_conversion_format = "3";
+            window.google_conversion_color = "ffffff";
+            window.google_conversion_label = config.conversion_label;
+            window.google_conversion_value = 0;
+
+            var oldDocWrite = document.write;
+
+            document.write = function(node){
+                $("body").append(node)
+            }
+
+            $.getScript("http://www.googleadservices.com/pagead/conversion.js", function() {
+
+                setTimeout(function() {
+                    document.write = oldDocWrite
+                }, 100)
+
+            })
+        }
+        function addRemarketing(){
+			googleTrackingPixel(_config);
 		}
-		function lookForBadge(){
-			addRemarketing(_config.remarketing_code);
-		}
-		lookForBadge();
+        addRemarketing();
     }
 )
